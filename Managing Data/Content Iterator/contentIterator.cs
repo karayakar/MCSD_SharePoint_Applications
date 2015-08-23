e3b983867044bc94871ac996e9fcd9efa96f7cac
Shaun Lewis <lewis.shaun1@outlook.com>
2015-08-23 09:56:56 -0400

    commit

---
+++ Managing Data/Content Iterator/contentIterator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.SharePoint;
+using Microsoft.Office.Server.Utilities;
+
+public partial class test : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnIterator_Click(object sender, EventArgs e)
+    {
+        using (SPSite site = new SPSite(SPContext.Current.Web.Url)) ;
+        using (SPWeb web = site.OpenWeb())
+        {
+            SPList list = web.Lists.TryGetList("LargeList");
+            if(list != null)
+            {
+                SPQuery camlQuery = new SPQuery();
+                camlQuery.ViewFields = @"<FieldRef Name =""Title""/><FieldRef Name=""Description""/>";
+                camlQuery.RowLimit = 8000;
+
+                ContentIterator iterator = new ContentIterator();
+                iterator.ProcessListItems(list, camlQuery, ProcessItem, ProcessError);
+                grdData.DataSource = table;
+                grdData.DataBind();
+            }
+        }
+    }
+
+    private void ProcessItem(SPListItem item)
+    {
+        string title = item["Title"].ToString();
+        string description = item["Description"].ToString();
+        table.Rows.Add(title, description);
+
+    }
+
+    private void ProcessError(SPListItem item, Exception e)
+    {
+        throw new Exception("An error occured ", e);
+    }
+}
---
+++ PowerShell/Lists/Add_List_Items.ps1
@@ -0,0 +1,20 @@
+﻿$webURL = "http://abcuniversity.com"
+$listName = "LargeList"
+
+$web = Get-SPWeb $webUrl
+$list = $web.Lists[$listName]
+
+for($i=1; $i -le 50000; $i++)
+{
+    $newItem = $list.Items.Add()
+    $title = ""
+    for($j=0; $j -le 5; $j++)
+    {
+        $randomChar = [Char](Get-Random -Minimum 65 -Maximum 90)
+        $title += $randomChar
+
+    }
+    $newItem["Title"] = $title
+    $newItem["Description"] = "Item number " + $i
+    $newItem.Update()
+}
