2bb83b5cb09e0c50d4ee9b1ce752a7300ff53798
Shaun Lewis <lewis.shaun1@outlook.com>
2015-09-19 21:19:34 -0400

    commit

---
+++ Managing Solutions/Solution Upgrade/README.md
@@ -0,0 +1,10 @@
+﻿#Feature Dependencies
+
+	Modify below element for feature dependency
+	<ActivationDependency FeatureId="dc7ce12f-5bc8-400b-b178-4c595ddbac3f" FeatureTitle="World Cup Definitions" />
+	Go to FEATURES folder in <hive>/TEMPLATES/FEATURES
+
+#Add Feature Dependency
+
+	1. (Visual Studio) Create Feature
+	2. Add Feature Deactivation Dependencies(Add)
---
+++ Managing Solutions/Solution Upgrade/solutionUpgrade.ps1
@@ -0,0 +1,10 @@
+﻿Uninstall-SPSolution -Identity "anywsp.wsp" -Confirm:$false
+Remove-SPSolution -Identity "anywsp.wsp" -Confirm:$false
+
+Add-SPSolution -LiteralPath "C:\absolutePath\toanywsp.wsp"
+Install-SPSolution -Identity "anywsp.wsp" -GACDeployment
+
+Enable-SPFeature -Url http://budostylz.sharepoint.com
+Enable-SPFeature -Url http://budostylz.sharepoint.com
+
+Update-SPSolution -Identity "anywsp.wsp" -LiteralPath "C:\absolutePath\anywsp.wsp"
