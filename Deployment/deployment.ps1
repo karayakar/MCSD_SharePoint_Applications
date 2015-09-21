02ba2eb71f3b4438a297ee4497dab0f2797c608f
Shaun Lewis <lewis.shaun1@outlook.com>
2015-09-21 19:52:35 -0400

    'commit'

--- Deployment/deployment.ps1
+++ Deployment/deployment.ps1
@@ -3,4 +3,4 @@ Install-SPSolution -Idenity "solution.wsp" -GACDeployment
 #check timer job /_admin/ServiceRunningJobs.aspx
 
 Uninstall-SPSolution -Identity "solution.wsp" -Confirm:$false
-Remove-SPSOlution -Identity "solution.wsp" -Confirm:$false
+Remove-SPSolution -Identity "solution.wsp" -Confirm:$false
