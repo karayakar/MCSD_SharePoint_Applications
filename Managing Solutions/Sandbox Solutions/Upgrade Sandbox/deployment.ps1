e684262035542f59c9d47d0b963fbe481d23a232
Shaun Lewis <lewis.shaun1@outlook.com>
2015-09-22 20:17:04 -0400

    'commit'

---
+++ Managing Solutions/Sandbox Solutions/Upgrade Sandbox/deployment.ps1
@@ -0,0 +1,6 @@
+﻿Add-SPSolution -LiteralPath "C:\solutionsFolder\appendNewVersionNameHere.wsp"
+Install-SPSolution -Identity "solution.wsp" -GACDeployment
+#check timer job /_admin/ServiceRunningJobs.aspx
+
+Uninstall-SPSolution -Identity "solution.wsp" -Confirm:$false
+Remove-SPSOlution -Identity "solution.wsp" -Confirm:$false
