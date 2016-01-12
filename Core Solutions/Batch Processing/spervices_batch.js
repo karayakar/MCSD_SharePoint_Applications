6a0ffefdd83a45bb43f4e33daccafa59d9426bf8
Shaun Lewis <lewis.shaun1@outlook.com>
2016-01-12 15:05:41 -0500

    Create spervices_batch.js

---
+++ Core Solutions/Batch Processing/spervices_batch.js
@@ -0,0 +1,38 @@
+//new list items
+$().SPServices({
+								        operation: "UpdateListItems",
+								        async: false,
+								        batchCmd: "New",
+								        listName: source_list,
+								        valuepairs: [["Key", Value]],
+							        	completefunc: function(xData, Status) {
+							          		//console.log("completed");
+							        	}
+	});
+
+
+//update list items
+$().SPServices({
+								        operation: "UpdateListItems",
+								        ID: 1,
+								        async: false,
+								        batchCmd: "Update",
+								        listName: source_list,
+								        valuepairs: [["Key", Value]],
+							        	completefunc: function(xData, Status) {
+							          		//console.log("completed");
+							        	}
+	});
+	
+	//delete items
+	$().SPServices({
+								        operation: "UpdateListItems",
+								        ID: 1,
+								        async: false,
+								        batchCmd: "Delete",
+								        listName: source_list,
+								        valuepairs: [["Key", Value]],
+							        	completefunc: function(xData, Status) {
+							          		//console.log("completed");
+							        	}
+	});
